53fb6b07eb73e77c41c2a6251b1a4f28825bfc86
dani3lthorpe <dani3lthorpe@gmail.com>
2024-08-22 15:12:36 +1200

    Added Styles

--- components/ShoppingList.js
+++ components/ShoppingList.js
@@ -1,45 +1,67 @@
 import React from 'react';
-import { View, StyleSheet, Text, FlatList, Button } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import { fontSizes, spacing } from './utils/sizes';
 
 export const ShoppingList = ({ list, deleteItem }) => {
   if (!list || !list.length)
-    return <Text style={styles.item}>No Items in ShoppingList</Text>;
+    return (
+      <Text style={styles.empty}>There are no items in your ShoppingList</Text>
+    );
 
   const renderItem = ({ item }) => (
     <View style={styles.listItem}>
       <Text style={styles.item}>- {item.name}</Text>
-      <Button style={styles.button} title="Delete" onPress={() => deleteItem(item)} />
+      <TouchableOpacity style={styles.button} onPress={() => deleteItem(item)}>
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
     </View>
   );
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Shopping List:</Text>
       <FlatList data={list} renderItem={renderItem} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
- container: {
-    padding: spacing.md,
+  container: {
+    paddingLeft: spacing.md,
+    paddingRight: spacing.md,
     flex: 1,
   },
-  listItem: 
-  {
-        flexDirection: 'row',
+  listItem: {
+    flexDirection: 'row',
+  },
+  empty: {
+    textAlign: 'center',
+    fontSize: fontSizes.md,
+    paddingTop: spacing.sm,
+    flex: 1,
+    color: '#F7E7DC',
   },
   item: {
     fontSize: fontSizes.md,
     paddingTop: spacing.sm,
-    flex: 1
-  },
-  title: {
-    fontSize: fontSizes.md,
-    fontWeight: 'bold',
+    flex: 1,
+    color: '#F7E7DC',
   },
   button: {
-    justifyContent: 'space-between'
-  }
+    justifyContent: 'space-between',
+    backgroundColor: '#F7E7DC',
+    marginBottom: spacing.sm,
+  },
+  buttonText: {
+    color: '#405D72',
+    fontSize: fontSizes.md,
+    paddingVertical: spacing.sm,
+    paddingHorizontal: spacing.md,
+    borderRadius: 20,
+  },
 });
--- components/ShoppingListDisplay.js
+++ components/ShoppingListDisplay.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { ShoppingList } from './ShoppingList';
 import { fontSizes, spacing } from './utils/sizes';
@@ -13,18 +13,18 @@ export const ShoppingListDisplay = () => {
       const newItem = { id: Date.now(), name: subject };
       setList([...list, newItem]);
       setSubject('');
-      {
-        console.log(list);
-      }
     }
   };
 
-    const removeItem = (item) => {
-      setList(list.filter((x) => x.id !== item.id));
-    };
-    
+  const removeItem = (item) => {
+    setList(list.filter((x) => x.id !== item.id));
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Your Shopping List</Text>
+      </View>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
@@ -32,29 +32,60 @@ export const ShoppingListDisplay = () => {
           value={subject}
           label="add Item"
         />
-        <View style={styles.button}>
-          <Button title="Add" size={50} onPress={addItemToList} />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} size={50} onPress={addItemToList}>
+            <Text style={styles.buttonText}>Add</Text>
+          </TouchableOpacity>
         </View>
       </View>
-      <ShoppingList list={list} deleteItem={removeItem} />
+      <View style={styles.listContainer}>
+        <ShoppingList list={list} deleteItem={removeItem} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    backgroundColor: '#405D72',
+    flex: 1,
   },
-  button: {
+  buttonContainer: {
     justifyContent: 'center',
+    backgroundColor: '#F7E7DC',
   },
   textInput: {
     flex: 1,
     marginRight: spacing.sm,
+    backgroundColor: '#F7E7DC',
+    color: '#405D72',
   },
   inputContainer: {
     padding: spacing.lg,
     justifyContent: 'top',
     flexDirection: 'row',
+  },
+  listContainer: {
+    flex: 1,
+    padding: spacing.sm,
+  },
+  titleContainer: {
+    paddingTop: spacing.md,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: fontSizes.xxl,
+    color: '#FFA726',
+  },
+    button: {
+    justifyContent: 'space-between',
+    backgroundColor: '#F7E7DC',
+  },
+  buttonText: {
+    color: '#405D72',
+    fontSize: fontSizes.md,
+    paddingVertical: spacing.sm,
+    paddingHorizontal: spacing.md,
+    borderRadius: 20,
   },
 });
