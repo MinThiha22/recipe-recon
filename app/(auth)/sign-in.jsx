7259a1f68ccd399d9f0550db3dd7b729fca47fdb
Min Thiha Ko Ko (Mike) <minthihakoko@gmail.com>
2024-09-13 03:44:02 +1200

    Edit page title

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -5,9 +5,8 @@ const SignIn = () => {
   return (
     <SafeAreaView className="h-full bg-primary">
       <View className="flex-1 items-center justify-center">
-        <Text className="text-3xl text-red">Home!</Text>
-        <Link href="/sign-up" className="text-title underline text-lg font-chewy pt-4">Sign Up</Link>
-
+      <Text className="text-3xl text-red">Sign In Page!</Text>
+      <Link href="/sign-up" className="text-title underline text-lg font-chewy pt-4">Go to Sign Up</Link>
       
       </View>
     </SafeAreaView>
