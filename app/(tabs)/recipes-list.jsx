fb018ff3cc06495eedf58c28931584e46e5e7ee6
Min Thiha Ko Ko (Mike) <minthihakoko@gmail.com>
2024-09-15 22:07:26 +1200

    Design the index page

--- app/(tabs)/recipes-list.jsx
+++ app/(tabs)/recipes-list.jsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
 
 const RecipesList = () => {
   return (
-    <View>
-      <Text>RecipesList</Text>
-    </View>
+    <SafeAreaView className="h-full bg-primary">
+      <View className="w-full min-h-[80vh] items-center justify-center">
+        <Text className="text-3xl mt-5 text-title font-chewy">
+            Recipes-List
+        </Text>
+      </View>
+      <StatusBar backgroundColor='#161622' style='light'></StatusBar>
+    </SafeAreaView>
   )
 }
 
