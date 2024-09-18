f590eb4241136cf87738530cee0c0c1367fe42f8
dani3lthorpe <dani3lthorpe@gmail.com>
2024-09-18 12:13:54 +1200

    fixed merge errors

--- constants/images.js
+++ constants/images.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import whiteLogo from "../assets/images/logo_white.png";
 import cooking from "../assets/images/cooking.gif"
 
 export default {whiteLogo, cooking};
-=======
-import main from "../assets/images/logo.png";
-import whiteLogo from "../assets/images/logo_white.png";
-
-export default {main, whiteLogo};
->>>>>>> login
