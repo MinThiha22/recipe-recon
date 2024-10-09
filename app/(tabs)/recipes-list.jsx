bf0941c33fbb9cecc113cd983c3c70de7a146b2f
Droun-Mattu <drounmattu@gmail.com>
2024-10-09 14:18:34 +1300

    Instruction page successfully implemented

--- app/(tabs)/recipes-list.jsx
+++ app/(tabs)/recipes-list.jsx
@@ -1,16 +1,25 @@
-import { useState, useEffect } from 'react';
-import { View, Text, TextInput, FlatList, Image, TouchableOpacity, Modal, SafeAreaView } from 'react-native';
-import axios from 'axios';
-import RenderHtml from 'react-native-render-html';
-import { db, auth } from '../../lib/firebase.js';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
-import Instructions from '../../components/Instructions.jsx';
+import { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Modal,
+  SafeAreaView,
+} from "react-native";
+import axios from "axios";
+import RenderHtml from "react-native-render-html";
+import { db, auth } from "../../lib/firebase.js";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import Instructions from "../../components/Instructions.jsx";
 
 const RecipeList = () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
   const [favouriteList, setFavouriteList] = useState([]);
@@ -23,49 +32,46 @@ const RecipeList = () => {
   const searchRecipes = async (currentIsSortByIngredients) => {
     setRecipes();
     setLoading(true);
-    setError('');
+    setError("");
 
     //refresh ingredients
     if (user && currentIsSortByIngredients) {
       await fetchIngredients(user.uid);
     }
 
-    let ingredients = ingredientsList.join(',');
+    let ingredients = ingredientsList.join(",");
     let endpoint = null;
     let param = {};
     let sort = null;
     if (query.trim()) {
       //if search bar is not empty and sorting by ingredients and query, if not sort by query
-      endpoint = 'https://roughy-polite-wholly.ngrok-free.app/api/recipeSearch';
+      endpoint = "https://roughy-polite-wholly.ngrok-free.app/api/recipeSearch";
 
-      ingredients = currentIsSortByIngredients
-      ? ingredientsList.join(',')
-      : '';
+      ingredients = currentIsSortByIngredients ? ingredientsList.join(",") : "";
 
       sort = currentIsSortByIngredients
-      ? 'min-missing-ingredients'
-      : 'popularity';
-      
+        ? "min-missing-ingredients"
+        : "popularity";
+
       param = { query, ingredients, sort };
     } else {
       //if search bar is empty and sorting by ingredients use ingredients search endpoint, if sorting by ingredients use random endpoint
       endpoint = currentIsSortByIngredients
-        ? 'https://roughy-polite-wholly.ngrok-free.app/api/ingredientsSearch'
-        : 'https://roughy-polite-wholly.ngrok-free.app/api/recipeSearch/random';
+        ? "https://roughy-polite-wholly.ngrok-free.app/api/ingredientsSearch"
+        : "https://roughy-polite-wholly.ngrok-free.app/api/recipeSearch/random";
 
-      //if sorting by ingredients use ingredients parameter else use no paramters 
-      param = currentIsSortByIngredients
-        ? { ingredients }
-        : {};
+      //if sorting by ingredients use ingredients parameter else use no paramters
+      param = currentIsSortByIngredients ? { ingredients } : {};
     }
 
     //get data from server endpoint using parameters
     try {
       const response = await axios.get(endpoint, { params: param });
-      const recipeData = response.data.recipes || response.data.results || response.data;
+      const recipeData =
+        response.data.recipes || response.data.results || response.data;
       setRecipes(recipeData);
     } catch (err) {
-      setError('Failed to fetch recipes');
+      setError("Failed to fetch recipes");
     } finally {
       setLoading(false);
     }
@@ -79,16 +85,19 @@ const RecipeList = () => {
   //get specific recipe information from server when recipe is pressed
   const imagePressed = async (id) => {
     try {
-      const response = await axios.get(`https://roughy-polite-wholly.ngrok-free.app/api/recipeInfo`, {
-        params: { query: id },
-      });
+      const response = await axios.get(
+        `https://roughy-polite-wholly.ngrok-free.app/api/recipeInfo`,
+        {
+          params: { query: id },
+        }
+      );
       const recipeInfo = response.data;
       console.log(recipeInfo);
       setSelectedRecipe(recipeInfo);
       setModalVisible(true);
       addRecents(recipeInfo);
     } catch (err) {
-      setError('Failed to fetch recipes');
+      setError("Failed to fetch recipes");
     } finally {
       setLoading(false);
     }
@@ -114,7 +123,7 @@ const RecipeList = () => {
 
   //get favourites from firebase
   const fetchFavourites = async (userId) => {
-    const docRef = doc(db, 'favourites', userId);
+    const docRef = doc(db, "favourites", userId);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
@@ -124,13 +133,12 @@ const RecipeList = () => {
 
   //get ingredients from firebase
   const fetchIngredients = async (userId) => {
-    const docRef = doc(db, 'ingredients', userId);
+    const docRef = doc(db, "ingredients", userId);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
       setIngredientsList(docSnap.data().list);
-    }
-    else {
+    } else {
       setIngredientsList([]);
     }
   };
@@ -138,7 +146,7 @@ const RecipeList = () => {
   //save favourites to firebase
   const saveFavourites = async (updatedList) => {
     if (user) {
-      const userFavouritesRef = doc(db, 'favourites', user.uid);
+      const userFavouritesRef = doc(db, "favourites", user.uid);
       await setDoc(userFavouritesRef, { list: updatedList });
     }
   };
@@ -174,7 +182,7 @@ const RecipeList = () => {
 
   //toggle ingredient sort and update the search
   const toggleIngredientsSort = () => {
-    setIsSortByIngredients(prevIsSortByIngredients => {
+    setIsSortByIngredients((prevIsSortByIngredients) => {
       const newIsIngredients = !prevIsSortByIngredients;
       searchRecipes(newIsIngredients);
       return newIsIngredients;
@@ -183,7 +191,7 @@ const RecipeList = () => {
 
   //get recents from firebase
   const fetchRecents = async (userId) => {
-    const docRef = doc(db, 'recents', userId);
+    const docRef = doc(db, "recents", userId);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
@@ -194,7 +202,7 @@ const RecipeList = () => {
   //save recents to firebase
   const saveRecents = async (updatedList) => {
     if (user) {
-      const userRecentsRef = doc(db, 'recents', user.uid);
+      const userRecentsRef = doc(db, "recents", user.uid);
       await setDoc(userRecentsRef, { list: updatedList });
     }
   };
@@ -214,7 +222,9 @@ const RecipeList = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <View className="flex-1 p-4">
-        <Text className="text-5xl font-chewy text-center text-title pt-5">Recipe Search</Text>
+        <Text className="text-5xl font-chewy text-center text-title pt-5">
+          Recipe Search
+        </Text>
         <TextInput
           className="border border-gray-300 rounded p-2 mb-4 text-secondary"
           placeholder="Enter ingredients"
@@ -235,10 +245,18 @@ const RecipeList = () => {
           onPress={toggleIngredientsSort}
           disabled={loading}
         >
-          <Text className="text-white font-bold text-center">{isSortByIngredients ? 'Unsort by your ingredients' : 'Sort by your ingredients'}</Text>
+          <Text className="text-white font-bold text-center">
+            {isSortByIngredients
+              ? "Unsort by your ingredients"
+              : "Sort by your ingredients"}
+          </Text>
         </TouchableOpacity>
 
-        {loading && <Text className="text-3xl pt-10 font-chewy text-center text-title">Loading...</Text>}
+        {loading && (
+          <Text className="text-3xl pt-10 font-chewy text-center text-title">
+            Loading...
+          </Text>
+        )}
         {error && <Text className="text-red-500 mb-4">{error}</Text>}
 
         <FlatList
@@ -247,17 +265,32 @@ const RecipeList = () => {
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <View className="mb-4 bg-primary">
-              <TouchableOpacity className="flex-1 items-center justify-center" onPress={() => imagePressed(item.id)
-              }>
+              <TouchableOpacity
+                className="flex-1 items-center justify-center"
+                onPress={() => imagePressed(item.id)}
+              >
                 <Image className="w-60 h-60" source={{ uri: item.image }} />
-                <Text className="text-2xl font-chewy text-center text-title">{item.title}</Text>
+                <Text className="text-2xl font-chewy text-center text-title">
+                  {item.title}
+                </Text>
                 {isSortByIngredients && (
                   <>
                     <Text className="text-md font-poppingsRegular text-center text-secondary">
-                      Ingredients: {item.usedIngredients && item.usedIngredients.length > 0 ? item.usedIngredients.map(ingredient => ingredient.name).join(', ') : ""}
+                      Ingredients:{" "}
+                      {item.usedIngredients && item.usedIngredients.length > 0
+                        ? item.usedIngredients
+                            .map((ingredient) => ingredient.name)
+                            .join(", ")
+                        : ""}
                     </Text>
                     <Text className="text-md font-poppingsRegular text-center text-secondary">
-                      {item.missedIngredients && item.missedIngredients.length > 0 ? "Missing Ingredients: " + item.missedIngredients.map(ingredient => ingredient.name).join(', ') : ""}
+                      {item.missedIngredients &&
+                      item.missedIngredients.length > 0
+                        ? "Missing Ingredients: " +
+                          item.missedIngredients
+                            .map((ingredient) => ingredient.name)
+                            .join(", ")
+                        : ""}
                     </Text>
                   </>
                 )}
@@ -274,8 +307,13 @@ const RecipeList = () => {
             onRequestClose={closeModal}
           >
             <View className="flex-1 justify-center items-center m-5 bg-secondary p-5 rounded-lg">
-              <Image className="w-48 h-48" source={{ uri: selectedRecipe.image }} />
-              <Text className="text-3xl font-chewy text-center text-title">{selectedRecipe.title}</Text>
+              <Image
+                className="w-48 h-48"
+                source={{ uri: selectedRecipe.image }}
+              />
+              <Text className="text-3xl font-chewy text-center text-title">
+                {selectedRecipe.title}
+              </Text>
               <RenderHtml
                 contentWidth={400}
                 source={{ html: selectedRecipe.summary }}
@@ -284,23 +322,27 @@ const RecipeList = () => {
                 className="bg-blue-500 p-3 rounded-full mt-4"
                 onPress={closeModal}
               >
-                <Text className="text-white font-bold text-center">Hide Modal</Text>
+                <Text className="text-white font-bold text-center">
+                  Hide Modal
+                </Text>
               </TouchableOpacity>
 
               <TouchableOpacity
                 className="bg-title p-3 rounded-full mt-4"
                 onPress={toggleFavourite}
               >
-                <Text className="text-white font-bold text-center">{isFavourite(selectedRecipe.id) ? 'Unfavourite' : 'Favourite!'}</Text>
+                <Text className="text-white font-bold text-center">
+                  {isFavourite(selectedRecipe.id)
+                    ? "Unfavourite"
+                    : "Favourite!"}
+                </Text>
               </TouchableOpacity>
-
-              <Instructions id={selectedRecipe.id}/>
-
+              {selectedRecipe && <Instructions recipe={selectedRecipe} />}
             </View>
           </Modal>
         )}
       </View>
-    </SafeAreaView >
+    </SafeAreaView>
   );
 };
 
--- components/Instructions.jsx
+++ components/Instructions.jsx
@@ -1,50 +1,78 @@
-import React from 'react';
-import { View, Text, FlatList, TouchableOpacity, Modal, SafeAreaView, Alert } from 'react-native';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  SafeAreaView,
+  Alert,
+  ScrollView,
+} from "react-native";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Instructions = ( {id} ) => {
-
-const [instructions, setInstructions] = useState([]);
-const [instructionsVisible, setInstructionsVisible] = useState(false);
-
-  const getInstructions = async (recipeId) => {
-    try {
-      const response = await axios.get(`https://roughy-polite-wholly.ngrok-free.app/api/recipeInstructions`, {
-        params: { query: id },
-      });
-      const instruction = response.data[0]?.steps;
-      setInstructions(instruction);
-      setInstructionsVisible(true);
-    } catch (err) {
-      Alert.alert("Failed to fetch instructions")
-    }
-  };
+const Instructions = ({ recipe }) => {
+  const [instructionsVisible, setInstructionsVisible] = useState(false);
 
   return (
     <View>
-     <Modal
-            animationType="slide"
-            transparent={true}
-            visible={instructionsVisible}
-            onRequestClose={() => (setInstructionsVisible(false))}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={instructionsVisible}
+        onRequestClose={() => setInstructionsVisible(false)}
+      >
+        <View className="flex-1 justify-center items-center m-5 bg-primary border-black p-5 rounded-xl shadow-lg">
+          <Text className="text-white font-bold text-center pb-5">
+            {recipe.title}
+          </Text>
+          <Text className="text-white font-bold text-center">Ingredients</Text>
+          <FlatList
+            className="pt-4"
+            data={recipe.extendedIngredients}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <View>
+                <Text className="text-2xl font-chewy text-center text-title">
+                  {item.nameClean}
+                </Text>
+                <Text className="text-md font-poppingsRegular text-center text-secondary">
+                  {item.measures.us.amount} {item.measures.us.unitLong}
+                </Text>
+              </View>
+            )}
+          />
+          <FlatList
+            className="pt-4"
+            data={recipe.analyzedInstructions[0]?.steps}
+            keyExtractor={(item) => item.number.toString()}
+            renderItem={({ item }) => (
+              <View>
+                <Text className="text-2xl font-chewy text-center text-title">
+                  Step {item.number}
+                </Text>
+                <Text className="text-md font-poppingsRegular text-center text-secondary">
+                  {item.step}
+                </Text>
+              </View>
+            )}
+          />
+          <TouchableOpacity
+            className="bg-blue-500 p-3 rounded-full mt-4"
+            onPress={() => setInstructionsVisible(false)}
           >
-            <View className="flex-1 justify-center items-center m-5 bg-secondary p-5 rounded-lg">
-              <TouchableOpacity
-                className="bg-blue-500 p-3 rounded-full mt-4"
-                onPress={() => (setInstructionsVisible(false))}
-              >
-                <Text className="text-white font-bold text-center">Hide Modal</Text>
-              </TouchableOpacity>
-            </View>
-          </Modal>
-    <TouchableOpacity
-    className="bg-title p-3 rounded-full mt-4"
-    onPress={() => (getInstructions(id))}
-  >
-    <Text className="text-white font-bold text-center">Instructions</Text>
-  </TouchableOpacity>
-  </View>
+            <Text className="text-white font-bold text-center">Hide Modal</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <TouchableOpacity
+        className="bg-title p-3 rounded-full mt-4"
+        onPress={() => setInstructionsVisible(true)}
+      >
+        <Text className="text-white font-bold text-center">Instructions</Text>
+      </TouchableOpacity>
+    </View>
   );
-}
-export default Instructions
+};
+export default Instructions;
